cdc8068fa682508a07149686599c1a2293a60f38
Tom Bowyer <tom4310@gmail.com>
2021-05-07 17:58:49 +1000

    Add updateNOtification id to seen

--- internal/db/update.go
+++ internal/db/update.go
@@ -6,6 +6,7 @@ import (
 	"github.com/TomBowyerResearchProject/common/logger"
 	commonMongo "github.com/TomBowyerResearchProject/common/mongo"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 func UpdateNotificationsSeen(link, username string) {
@@ -17,6 +18,24 @@ func UpdateNotificationsSeen(link, username string) {
 		bson.M{
 			"link":     link,
 			"username": username,
+		},
+		bson.D{
+			{"$set", bson.D{{"seen", true}}}, //nolint:govet
+		},
+	)
+	if err != nil {
+		logger.Error(err)
+	}
+}
+
+func UpdateNotificationID(id primitive.ObjectID) {
+	db := commonMongo.GetDatabase()
+	notifCollection := db.Collection(NotificationsCollection)
+
+	_, err := notifCollection.UpdateMany(
+		context.TODO(),
+		bson.M{
+			"_id": id,
 		},
 		bson.D{
 			{"$set", bson.D{{"seen", true}}}, //nolint:govet
